b3fc57dcf53e6890f1acdbdbb613baf31c05ba92
jerriker <befikerkassahuntessema@gmail.com>
2025-11-17 23:02:34 +0300

    add function to display the books in the screen

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const bookLibrary = document.querySelector('.book-library')
+const bookList = document.querySelector('.book-list')
+
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -16,7 +19,10 @@ function Book(title, author, pages, read) {
 function addBook(title, author, pages, read) {
     const newBook = new Book(title, author, pages, read)
     myLibrary.push(newBook)
+    displayBook(newBook)
+    createBookShelf(newBook)
 }
+
 
 function displayBook() {
     myLibrary.forEach(book => {
@@ -24,3 +30,40 @@ function displayBook() {
     })
 }
 
+function createBookShelf(book) {
+    const bookShelf = document.createElement('div')
+    bookShelf.classList.add('book-shelf')
+
+    const bookTitle = document.createElement('p')
+    bookTitle.textContent = `${book.title}`
+
+    const bookAuthor = document.createElement('p')
+    bookAuthor.textContent = `${book.author}`
+
+    const bookPages = document.createElement('p')
+    bookPages.textContent = `${book.pages}`
+
+    const bookReadStatus = document.createElement('p')
+
+    function update() {
+        bookReadStatus.textContent = book.read ? "Read" : "Not read"
+        bookReadStatus.style.color = book.read ? "Green" : "Red"
+    }
+    update();
+
+    bookReadStatus.addEventListener('click', () => {
+        book.read = !book.read;
+        update();
+    });
+
+    bookShelf.appendChild(bookTitle)
+    bookShelf.appendChild(bookAuthor)
+    bookShelf.appendChild(bookPages)
+    bookShelf.appendChild(bookReadStatus)
+
+    bookList.appendChild(bookShelf)
+}
+
+addBook("Atomic Habits", "James Clear", 200, true)
+addBook("Ego is the enemy", "Ryan Holiday", 300, false)
+addBook("The subtle arts of not giving a fuck", "Ryan Holiday", 200, true)
